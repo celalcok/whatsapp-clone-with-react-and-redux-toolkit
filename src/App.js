8e4ffffc2fcdadc2802d5886ea7ef67e423f7a40
Celal <47282963+celalcok@users.noreply.github.com>
2022-08-24 12:33:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Main from './components/main/Main';
+import {useState} from "react"
 
+import React, { useLayoutEffect } from 'react';
+
+function useWindowWidth() {
+  const [width, setWidth] = useState([0]);
+  useLayoutEffect(() => {
+    function updateWidth() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
+  return width;
+}
 function App() {
+
   return (
     <div className="app">
-        <Main/>
+     {useWindowWidth()>1024?<Main/>:<div className="message-for-main"><h1>Your screen width must be greater than 1024</h1></div>}
     </div>
   );
 }
